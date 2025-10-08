70a2d6c9694725721a600d5c5eef991a39331922
spavlenko774 <93788662+spavlenko774@users.noreply.github.com>
2025-10-09 00:24:21 +0500

    practice 2 modules updated

--- practice/02 Similarity search/modules/distance_profile.py
+++ practice/02 Similarity search/modules/distance_profile.py
@@ -25,6 +25,14 @@ def brute_force(ts: np.ndarray, query: np.ndarray, is_normalize: bool = True) ->
 
     dist_profile = np.zeros(shape=(N,))
 
-    # INSERT YOUR CODE
+    for i in range(N):
+        subsequence = ts[i:i + m]  # выделяем подпоследовательность
+
+        if is_normalize:
+            subsequence = z_normalize(subsequence)  # нормализация подпоследовательности
+            norm_query = z_normalize(query)  # нормализация запроса
+            dist_profile[i] = norm_ED_distance(subsequence, norm_query)  # вычисляем нормализованное расстояние
+        else:
+            dist_profile[i] = ED_distance(subsequence, query)  # обычное евклидово расстояние
 
     return dist_profile
