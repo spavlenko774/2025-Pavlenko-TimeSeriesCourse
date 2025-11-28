376b5e6fe2a92f58dbca418b5d9cd867af5dec40
spavlenko774 <spavlenko74@mail.ru>
2025-11-28 14:07:23 +0500

    feat (similarity search): complete part 1

---
+++ practice/02 Similarity search/modules/.ipynb_checkpoints/distance_profile-checkpoint.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from modules.utils import z_normalize
+from modules.metrics import ED_distance, norm_ED_distance
+
+
+def brute_force(ts: np.ndarray, query: np.ndarray, is_normalize: bool = True) -> np.ndarray:
+    """
+    Calculate the distance profile using the brute force algorithm
+
+    Parameters
+    ----------
+    ts: time series
+    query: query, shorter than time series
+    is_normalize: normalize or not time series and query
+
+    Returns
+    -------
+    dist_profile: distance profile between query and time series
+    """
+
+    n = len(ts)
+    m = len(query)
+    N = n-m+1
+
+    dist_profile = np.zeros(shape=(N,))
+
+    for i in range(N):
+        subsequence = ts[i:i + m]  # выделяем подпоследовательность
+
+        if is_normalize:
+            subsequence = z_normalize(subsequence)  # нормализация подпоследовательности
+            norm_query = z_normalize(query)  # нормализация запроса
+            dist_profile[i] = norm_ED_distance(subsequence, norm_query)  # вычисляем нормализованное расстояние
+        else:
+            dist_profile[i] = ED_distance(subsequence, query)  # обычное евклидово расстояние
+
+    return dist_profile
